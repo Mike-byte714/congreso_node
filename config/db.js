94fb14357cc1ffd269f4acf5501fe39552ab65a2
Mike-byte714 <michaelshaoran23@gmail.com>
2026-06-27 19:18:53 -0600

    Agregar SSL para conexión a Aiven

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false  // ⬅️ Necesario para Aiven
   },
   waitForConnections: true,
   connectionLimit: 10,
