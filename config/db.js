ad97e2a5cf746021a1c6430e8a847aabf679bacb
Mike-byte714 <michaelshaoran23@gmail.com>
2026-06-27 18:41:07 -0600

    Actualizar config/db.js

--- config/db.js
+++ config/db.js
@@ -7,6 +7,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
